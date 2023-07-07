142cd32879fee3183b586c1fdb62ef74fdbc3141
Dan Wismer <daniel.s.wismer@gmail.com>
2023-07-07 10:17:41 -0600

    docs, changed argument name to pu_1km

--- scripts/functions/fct_matrix_to_raster.R
+++ scripts/functions/fct_matrix_to_raster.R
@@ -1,23 +1,23 @@
 #' Writes national data rij matrix to raster.
 #' 
 #' @description
-#' The matrix_to_raster function takes the national-AOI rij matrix  and 
-#' populates a template raster by setting values based on the cell index. 
+#' The matrix_to_raster function takes the `natdata_intersect` rij matrix and 
+#' and converts it back to raster. 
 #' 
 #' @param ncc_1km_idx a [terra::rast] that has the NCC national grid index as values.
 #' 
 #' @param natdata_intersect a sparse matrix of class [dgCMatrix]. The 
 #' natdata-aoi intersect matrix.
 #' 
-#' @param aoi_1km0 an aoi [terra::rast]. All values must be set to 0. This raster is 
-#' used to crop the national-aoi raster back to the original aoi and also create
-#' the binary output where aoi values == 0 and national data == 1.
+#' @param pu_1km a 1km [terra::rast]. This is the project planning units and
+#'  is used to crop the converted `natdata_intersect` raster to the project
+#'  PU extent.
 #' 
 #' @param output_folder a [character] path to output raster layers.
 #' 
 #' @param prefix a [character] string to append before the raster name. Helpful
-#' for keeping standard naming. Ex. T_ECC_SAR_. Where T == theme, 
-#' ECCC_SAR == source.
+#' for keeping standard naming (ex. `T_NAT_IUCN_BIRD_`, where: T == theme,
+#' NAT == national and IUCN_BIRD == source.)
 #' 
 #' @param datatype a [character] string to define the raster output data type:
 #' `LOG1S`, `INT1S`, `INT1U`, `INT2S`, `INT2U`, `INT4S`, `INT4U`, `FLT4S`, 
@@ -25,7 +25,7 @@
 
 matrix_to_raster = function(ncc_1km_idx, 
                             natdata_intersect, 
-                            aoi_1km0, 
+                            pu_1km, 
                             output_folder, 
                             prefix, 
                             datatype) {
@@ -46,7 +46,7 @@ matrix_to_raster = function(ncc_1km_idx,
       natdata_raster[natdata_intersect["Idx",]] <- natdata_intersect[i,]
       names(natdata_raster) <- name
       ## crop raster to AOI
-      cropped <- terra::crop(natdata_raster, aoi_1km0)
+      cropped <- terra::crop(natdata_raster, pu_1km)
       ## write to disk
       writeRaster(cropped, paste0(output_folder, "/", prefix, name,".tif"),
                   overwrite = TRUE, datatype = datatype)
