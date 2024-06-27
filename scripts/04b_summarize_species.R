02ff220de787453a97bab728525689601fc003ab
Dan Wismer <daniel.s.wismer@gmail.com>
2024-06-27 10:18:17 -0600

    summ species script updates

--- scripts/04b_summarize_species.R
+++ scripts/04b_summarize_species.R
@@ -24,36 +24,36 @@ library(stringr)
 library(terra)
 
 ## Set folder paths ----
-prj_folder <- "C:/Data/PRZ/WTW_PROJECTS/SW_ONTARIO_V2" # <--- CHANGE FOR NEW WTW PROJECT
-nat_folder <- file.path(prj_folder, "National")
-tif_folder <- file.path(prj_folder, "Tiffs")
-tbl_folder <- file.path(nat_folder, "_Tables")
+PRJ_PATH <- "C:/Data/PRZ/WTW/SW_ONTARIO_V2" # <--- CHANGE TO LOCAL WTW PROJECT
+
+tif_path <- file.path(PRJ_PATH, "Tiffs")
+tbl_path <- file.path(PRJ_PATH, "National/_Tables")
 
 ## Set output csv ----
-output_csv <- file.path(prj_folder, "Tiffs/METADATA.csv")
+output_csv <- file.path(PRJ_PATH, "Tiffs/METADATA.csv")
 
 # Read-in metadata xlsx's ----
 ## species
-ECCC_CH_META <- read_excel(file.path(tbl_folder,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 1)
-ECCC_SAR_META <- read_excel(file.path(tbl_folder,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 2)
-IUCN_AMPH_META <- read_excel(file.path(tbl_folder,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 3)
-IUCN_BIRD_META <- read_excel(file.path(tbl_folder,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 4)
-IUCN_MAMM_META <- read_excel(file.path(tbl_folder,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 5)
-IUCN_REPT_META <- read_excel(file.path(tbl_folder,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 6)
-NSC_END_META <- read_excel(file.path(tbl_folder,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 7)
-NSC_SAR_META <- read_excel(file.path(tbl_folder,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 8)
-NSC_SPP_META <- read_excel(file.path(tbl_folder,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 9)
+ECCC_CH_META <- read_excel(file.path(tbl_path,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 1)
+ECCC_SAR_META <- read_excel(file.path(tbl_path,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 2)
+IUCN_AMPH_META <- read_excel(file.path(tbl_path,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 3)
+IUCN_BIRD_META <- read_excel(file.path(tbl_path,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 4)
+IUCN_MAMM_META <- read_excel(file.path(tbl_path,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 5)
+IUCN_REPT_META <- read_excel(file.path(tbl_path,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 6)
+NSC_END_META <- read_excel(file.path(tbl_path,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 7)
+NSC_SAR_META <- read_excel(file.path(tbl_path,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 8)
+NSC_SPP_META <- read_excel(file.path(tbl_path,  "WTW_NAT_SPECIES_METADATA.xlsx"), sheet = 9)
 ## other national themes, weights and includes
-FEATURES_META <- read_excel(file.path(tbl_folder,  "WTW_NAT_FEATURES_METADATA.xlsx"))
+FEATURES_META <- read_excel(file.path(tbl_path,  "WTW_NAT_FEATURES_METADATA.xlsx"))
 
 # List files in Tiffs folder ----
 tif_lst <- list.files(
-  tif_folder, pattern='.tif$', full.names = TRUE, recursive = FALSE
+  tif_path, pattern='.tif$', full.names = TRUE, recursive = FALSE
 )
 
 # List includes ----
 include_lst <- list.files(
-  tif_folder, pattern='^I.*\\.tif$', full.names = TRUE, recursive = FALSE
+  tif_path, pattern='^I.*\\.tif$', full.names = TRUE, recursive = FALSE
 )
 
 # Mosaic includes into one raster ----
